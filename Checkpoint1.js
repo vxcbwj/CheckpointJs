c8d027b950b4bec015f58337029126821c694329
vxcbwj <sarah.gacem5@gmail.com>
2025-07-17 00:05:34 +0100

    meow

---
+++ Checkpoint1.js
@@ -0,0 +1,15 @@
+function minMax(numbers) {
+  let min, max;
+  min = numbers[0];
+  max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+  }
+  console.log("Min:", min);
+  console.log("Max:", max);
+}
